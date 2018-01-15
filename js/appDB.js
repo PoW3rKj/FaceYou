07cd9250305d571a5d39c4ce5c6d0149cff0cf9f
Unknown <simojuve4ever@gmail.com>
2018-01-15 02:03:54 +0100

    Web Cam Login
    Added Login Page with FaceID auth

--- js/appDB.js
+++ js/appDB.js
@@ -53,14 +53,55 @@ function addUser(e) {
     }
 }
 
-function getImageUser() {
+function getImageUser(callback) {
     var transaction = db.transaction(["user"], "readonly");
     var objectStore = transaction.objectStore("user");
 
-//x is some value
-    var ob = objectStore.get("0");
+    //x is some value
+    var request = objectStore.get(1);
 
-    ob.onsuccess = function(e) {
-        return ob;
-    }
+    request.onsuccess = function(e) {
+        callback(request.result.faceLink);
+    };
+}
+
+function faceId(sourceImageUrl, callback) {
+    var subscriptionKey = "5dd2b7052f474b22b421bd9460d2eed3";
+    var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+    // Request parameters.
+    var params = {
+        "returnFaceId": "true",
+        "returnFaceLandmarks": "false",
+        "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
+    };
+
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+
+        // Request headers.
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        type: "POST",
+
+        // Request body.
+        data: '{"url": ' + '"' + sourceImageUrl + '"}',
+    })
+        .done(function(data) {
+            var obj = JSON.parse(JSON.stringify(data, null, 2));
+            /*$("#infos").append('<strong>Occhiali: </strong>'+ obj[0].faceAttributes.glasses);*/
+            /*console.log("faceId: " + obj[0].faceId);*/
+            callback(obj[0].faceId);
+
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+            alert(errorString);
+        });
 }
